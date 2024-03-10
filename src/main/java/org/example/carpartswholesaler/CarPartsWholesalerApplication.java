ed1c82b3cb6712751563d2378602821a53b518b0
gszp1 <grzybekmateusz@poczta.fm>
2024-03-10 20:11:33 +0100

    Initial commit.

---
+++ src/main/java/org/example/carpartswholesaler/CarPartsWholesalerApplication.java
@@ -0,0 +1,13 @@
+package org.example.carpartswholesaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPartsWholesalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPartsWholesalerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/carpartswholesaler/CarPartsWholesalerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carpartswholesaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsWholesalerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
