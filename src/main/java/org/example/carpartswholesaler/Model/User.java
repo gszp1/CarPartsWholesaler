fdd32b20dd4b095e5a94a7557150f87c015cebfc
gszp1 <grzybekmateusz@poczta.fm>
2024-03-11 00:43:41 +0100

    Adds User entity.

---
+++ src/main/java/org/example/carpartswholesaler/Model/User.java
@@ -0,0 +1,30 @@
+package org.example.carpartswholesaler.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String Username;
+
+    private String FirstName;
+
+    private String Surname;
+
+    private String Email;
+
+    private String PhoneNumber;
+}
