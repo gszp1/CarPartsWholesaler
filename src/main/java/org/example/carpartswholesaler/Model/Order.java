3af59abf9ed63057c068ff2be0ef3afb535cd73b
gszp1 <grzybekmateusz@poczta.fm>
2024-03-13 11:39:49 +0100

    Adds annotations to orders and order_details.

--- src/main/java/org/example/carpartswholesaler/Model/Order.java
+++ src/main/java/org/example/carpartswholesaler/Model/Order.java
@@ -1,4 +1,18 @@
 package org.example.carpartswholesaler.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
+    
 }
--- src/main/java/org/example/carpartswholesaler/Model/OrderDetails.java
+++ src/main/java/org/example/carpartswholesaler/Model/OrderDetails.java
@@ -1,4 +1,19 @@
 package org.example.carpartswholesaler.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_details")
 public class OrderDetails {
+
+
 }
